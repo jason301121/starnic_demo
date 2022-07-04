bb9d548cce8cfc5dcc4b4d6074cf11e0f7d930fc
Jason <jason301121@gmail.com>
2022-07-04 15:45:21 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  publicPath: '/starnic_demo/'
+  publicPath: '/starnic_demo/',
+  configureWebpack: {
+    //关闭 webpack 的性能提示
+    performance: {
+      hints: false
+    }
+  }
 }
